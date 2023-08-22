9fa39c971d9fef0ddb3928d0acf4cbc8f7e013a0
ryjacky <ryjacky@outlook.com>
2023-08-23 01:17:45 +0900

    made ActionPlugin an interface

--- src/plugin/ActionPlugin.ts
+++ src/plugin/ActionPlugin.ts
@@ -1,10 +1,3 @@
-import { PluginProperties } from './PluginProperties';
-
-export abstract class ActionPlugin {
-  protected constructor(
-    public pluginProperties: PluginProperties,
-  ) {
-  }
-
-  abstract onExecuted(args: Map<string, any>): void;
+export interface ActionPlugin {
+  onExecuted(args: Map<string, any>): void;
 }
