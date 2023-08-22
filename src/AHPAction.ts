5806be8268f438997a9801be9c72081f628ada12
ryjacky <ryjacky@outlook.com>
2023-08-22 22:35:08 +0900

    added args to AHPAction prototype

--- src/AHPAction.ts
+++ src/AHPAction.ts
@@ -1,7 +1,8 @@
 export abstract class AHPAction {
   protected constructor(
+    public args: Map<string, any>,
     public delay = 0,
-    public repeat = 1,
+    public repeat = 1
   ) {}
 
   abstract onRun(): void;
