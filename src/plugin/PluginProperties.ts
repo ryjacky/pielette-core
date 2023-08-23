b6aeca9e48fea974c09a0f8a31874f5617631cec
ryjacky <ryjacky@outlook.com>
2023-08-23 18:01:20 +0900

    Added parameter list option to plugin properties

--- src/plugin/PluginProperties.ts
+++ src/plugin/PluginProperties.ts
@@ -2,7 +2,8 @@ export interface PluginProperties {
   name: string,
   type: PluginType,
   description?: string,
-  author?: string
+  author?: string,
+  parameters?: Map<string, string>
 }
 
 export enum PluginType {
