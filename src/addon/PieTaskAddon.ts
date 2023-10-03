8e5a4460b65bd65d2b69ebdfebf06ee9059fb0b6
ryjacky <ryjacky@outlook.com>
2023-10-03 23:57:59 +0900

    added documentation

--- src/addon/PieTaskAddon.ts
+++ src/addon/PieTaskAddon.ts
@@ -1,6 +1,10 @@
 import { Addon } from "./Addon";
 
 export interface PieTaskAddon extends Addon {
+  /**
+   * List of arguments that is requested, each entry is formatted as:
+   * argumentName:type
+   */
   readonly receiveArgs?: string[];
 
   onExecuted(args: OnExecutedArgs): void;
