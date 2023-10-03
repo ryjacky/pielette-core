d5f1857df0c3194af8545dc0442b120ce5c47738
ryjacky <ryjacky@outlook.com>
2023-10-03 23:08:51 +0900

    refactored package name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Log } from './Log';
-import { PieTaskAddon, OnExecutedArgs } from './plugin/PieTaskAddon';
-import { PluginType, AddonProperties } from './plugin/AddonProperties';
+import { PieTaskAddon, OnExecutedArgs } from './addon/PieTaskAddon';
+import { PluginType, AddonProperties } from './addon/AddonProperties';
 
 export { PieTaskAddon, Log, PluginType, AddonProperties, OnExecutedArgs };
