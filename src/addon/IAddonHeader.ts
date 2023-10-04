0af6916fc1e5b049620e60912698d7500f49d1aa
ryjacky <ryjacky@outlook.com>
2023-10-04 12:23:23 +0900

    add receive arg type

--- src/addon/IAddonHeader.ts
+++ src/addon/IAddonHeader.ts
@@ -5,5 +5,7 @@ export interface IAddonHeader {
    * The list of arguments that the addon wants to receive. All arguments are passed as strings.
    * fileType specifies how the user can input the argument.
    * */
-  receiveArgs?: { argName: string, fieldType: string }[]
+  receiveArgs?: ReceiveArgs[]
 }
+
+export type ReceiveArgs = { argName: string; fieldType: "text" | "number" | "boolean" | "select" | "file" | "folder" | "shortcut"; }[];
